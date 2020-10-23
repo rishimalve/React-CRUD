b60662ef7cc2fa97a4b583b03966160e03462f5a
rishimalve <rishi.malve@gmail.com>
2020-10-23 19:39:57 -0400

    Updates

---
+++ precache-manifest.7bdc645c52a983bc86cf9d0f75f7a4ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cd570cdde775eb71fd408e46f9a9492",
+    "url": "/React-CRUD/index.html"
+  },
+  {
+    "revision": "c97365066a266a0ed122",
+    "url": "/React-CRUD/static/css/main.47903b21.chunk.css"
+  },
+  {
+    "revision": "3a64e34ab54e3a33c25a",
+    "url": "/React-CRUD/static/js/2.5e3acb1d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-CRUD/static/js/2.5e3acb1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c97365066a266a0ed122",
+    "url": "/React-CRUD/static/js/main.25eca67f.chunk.js"
+  },
+  {
+    "revision": "afbefa8f5c06566769ed",
+    "url": "/React-CRUD/static/js/runtime-main.e7251da5.js"
+  }
+]);
